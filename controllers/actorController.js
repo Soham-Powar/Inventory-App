61397f9bc2759e1395ef50e37a0ad7ff77bd5b8b
Soham Powar <173037437+Soham-Powar@users.noreply.github.com>
2025-08-30 21:40:32 +0530

    feat: setup actors new route with form and submit

--- controllers/actorController.js
+++ controllers/actorController.js
@@ -8,3 +8,21 @@ exports.actorsGet = async (req, res) => {
     console.error(err);
   }
 };
+
+exports.actorsNewGet = async (req, res) => {
+  res.render("actors/new");
+};
+
+exports.actorsNewPost = async (req, res) => {
+  const { name, birth_year, nationality } = req.body;
+  console.log(req.body);
+  try {
+    await db.addActor({ name, birth_year, nationality });
+    //gap in the db id genre
+    res.redirect("/actors");
+  } catch (err) {
+    //do proper error handling
+    res.redirect("/actors");
+    console.error(err);
+  }
+};
--- routes/actorRouter.js
+++ routes/actorRouter.js
@@ -3,5 +3,7 @@ const actorRouter = new Router();
 const actorController = require("../controllers/actorController");
 
 actorRouter.get("/", actorController.actorsGet);
+actorRouter.get("/new", actorController.actorsNewGet);
+actorRouter.post("/new", actorController.actorsNewPost);
 
 module.exports = actorRouter;
